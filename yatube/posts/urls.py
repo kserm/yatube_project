ac8c1d9eeec3bde52ee9d1f95dc8567defdcc5d0
Konstantin Ermachkov <kermachkov@gmail.com>
2022-05-05 21:37:19 +0300

    added database, static and updated templates

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'posts'
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='main-view'),
     # path('groups/', views.groups_list),
-    path('group/<slug:slug>/', views.group_posts),
+    path('group/<slug:slug>/', views.group_posts, name='group_posts'),
+    path('group_list/', views.group_list, name='group_list'),
 ]
